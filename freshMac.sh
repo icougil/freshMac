a273f14594d6224b156397a7af83aceb35b38c32
Nacho Cougil <nacho@codurance.com>
2017-10-07 23:18:43 +0200

    Included many pending soft: ansible, completions, htop, pip, rbenv, tor, terraform, vault, etc

--- freshMac.sh
+++ freshMac.sh
@@ -39,8 +39,6 @@ brew cask cleanup
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-rbenv init
-
 CMD="sdk install"
 apps=( java maven groovy gradle )
 for app in "${apps[@]}"
@@ -48,8 +46,55 @@ do
 	$CMD $app
 done
 
+brew install "maven-completion"
+
 # iterm2 - integration and utilities
 curl -L https://iterm2.com/misc/install_shell_integration_and_utilities.sh | bash
 
 sudo easy_install pip
 sudo pip install virtualenv
+
+# upgrade gems
+sudo gem update --system
+
+# install updated ruby versions (most updated that cames with macOS)
+# rbenv - https://github.com/rbenv/rbenv#homebrew-on-macos
+echo "Setting up rbenv..."
+rbenv init
+echo "Now, close this Terminal and open a new one so your changes take effect"
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor | bash
+rbenv install 2.4.1
+rbenv local 2.4.1
+# install jekyll
+gem install jekyll bundler
+
+# mega.nz
+curl -fsSL https://mega.nz/MEGAsyncSetup.dmg -o MEGAsyncSetup.dmg && open MEGAsyncSetup.dmg
+
+# Tor browser
+TOR_VERSION="7.0.5"
+curl -fsSL https://github.com/TheTorProject/gettorbrowser/releases/download/v$TOR_VERSION/TorBrowser-$TOR_VERSION-osx64_en-US.dmg -o TorBrowser.dmg && open TorBrowser.dmg
+
+# rust
+curl https://sh.rustup.rs -sSf | sh
+
+# lines of code
+cargo install loc
+
+# pyenv install env
+pyenv install 2.7.13
+pyenv local 2.7.13
+
+# youtoube upload - https://github.com/tokland/youtube-upload
+pip install --upgrade google-api-python-client progressbar2
+easy_install progressbar
+wget https://github.com/tokland/youtube-upload/archive/master.zip
+unzip master.zip
+cd youtube-upload-master
+python setup.py install
+cd ..
+rm -rf youtube-upload-master/
+rm master.zip
+
+# tocdoc
+npm install -g doctoc
